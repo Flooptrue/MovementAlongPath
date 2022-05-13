18b5c928aba92d38b16ac79d06a85528878a50ee
Artem Venger <artemvengertilshift@gmail.com>
2022-05-13 19:45:27 +0700

    Скопировал скрипт создания Mesh'а
    Скопировал из Path Creator

---
+++ Assets/Scripts/PathSceneTool.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using PathCreation;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public abstract class PathSceneTool : MonoBehaviour
+{
+    #region SerializeFields
+
+    [SerializeField] protected PathCreator _pathCreator;
+    [SerializeField] protected bool        _autoUpdate = true;
+
+    #endregion
+    
+    #region PublicAPI
+
+    public event Action Destroyed;
+    
+    public void TriggerUpdate()
+    {
+        PathUpdated();
+    }
+
+    #endregion
+
+    #region Logics
+
+    protected VertexPath path => _pathCreator.path;
+    
+    protected virtual void OnDestroy()
+    {
+        if (Destroyed != null)
+        {
+            Destroyed();
+        }
+    }
+
+    protected abstract void PathUpdated();
+
+    #endregion
+}
