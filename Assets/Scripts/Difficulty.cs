05a7aa112fc725daa503f478c34375028d1e0e5e
Artem Venger <artemvengertilshift@gmail.com>
2022-07-06 20:06:52 +0700

    Реализовал уничтожение препятствий

--- Assets/Scripts/Difficulty.cs
+++ Assets/Scripts/Difficulty.cs
@@ -4,10 +4,28 @@ public class Difficulty : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
+    private float _lifeTime;
+    private float _elapsedTime;
+
+    public void Init(float lifeTime)
+    {
+        _lifeTime = lifeTime;
+    }
+
     private void FixedUpdate()
     {
         var distance = _speed * Time.fixedDeltaTime;
         var t = transform;
         t.position += t.forward * distance;
+    }
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        if (_elapsedTime >= _lifeTime)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private float      _interval;
+    [SerializeField] private float      _lifeTime;
     [SerializeField] private Difficulty _difficulty;
 
     private IEnumerator Start()
     {
         while (true)
         {
-            var t = transform;
-            Instantiate(_difficulty, t.position, t.rotation);
+            var t        = transform;
+            var instance = Instantiate(_difficulty, t.position, t.rotation);
+            instance.Init(_lifeTime);
+
             yield return new WaitForSeconds(_interval);
         }
     }
