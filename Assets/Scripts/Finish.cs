063a79d2da7e65aa5dcdd38507fdeb5d0d22ccdf
Artem Venger <artemvengertilshift@gmail.com>
2022-07-06 21:06:22 +0700

    Указал размер вознаграждения

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Finish : MonoBehaviour
 {
+    [SerializeField] private float _reward;
+
     private void OnTriggerEnter(Collider other)
     {
         var player = other.attachedRigidbody.GetComponent<WaypointMover>();
