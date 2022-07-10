10beec9c9a90769aa868d3da70f76c8cc17102a9
Artem Venger <artemvengertilshift@gmail.com>
2022-07-10 20:47:53 +0700

    Создал скрипт для участка дороги

---
+++ Assets/Scripts/BitOfRoad.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BitOfRoad : MonoBehaviour
+{
+    #region SerializeFields
+
+    [SerializeField] private Direction _direction;
+
+    #endregion
+
+    #region Direction
+
+    public enum Direction
+    {
+        Same,
+        Reverse
+    }
+
+    #endregion
+}
