4dff97c09973ec29e662eab9519d550e0648ae3e
Artem Venger <artemvengertilshift@gmail.com>
2022-07-17 11:29:00 +0700

    Поместил логику в метод

--- Assets/Scripts/Rule.cs
+++ Assets/Scripts/Rule.cs
@@ -18,18 +18,23 @@ public class Rule : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.D))
         {
-            var message = new StringBuilder();
-
-            var totalDistance = 0f;
-            for (var i = 0; i < _points.Length - 1; i++)
-            {
-                var localDistance = Vector3.Distance(_points[i].position, _points[i + 1].position);
-                totalDistance += localDistance;
-                message.AppendLine($"[{i}-{i + 1}] {localDistance}");
-            }
-
-            message.Insert(0, $"TotalDistance {totalDistance}\n");
-            Debug.Log(message);
+            PrintDistance();
         }
+    }
+
+    private void PrintDistance()
+    {
+        var message = new StringBuilder();
+
+        var totalDistance = 0f;
+        for (var i = 0; i < _points.Length - 1; i++)
+        {
+            var localDistance = Vector3.Distance(_points[i].position, _points[i + 1].position);
+            totalDistance += localDistance;
+            message.AppendLine($"[{i}-{i + 1}] {localDistance}");
+        }
+
+        message.Insert(0, $"TotalDistance {totalDistance}\n");
+        Debug.Log(message);
     }
 }
