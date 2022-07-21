8df6a801a9bc3776f48f2ad8af99cb39c794753a
Artem Venger <artemvengertilshift@gmail.com>
2022-07-22 05:54:22 +0700

    Получил образец для создания объекта

--- Assets/Scripts/Editor/RuleEditor.cs
+++ Assets/Scripts/Editor/RuleEditor.cs
@@ -3,4 +3,10 @@ using UnityEditor;
 [CustomEditor(typeof(Rule))]
 public class RuleEditor : Editor
 {
+    private SerializedProperty _sample;
+
+    private void OnEnable()
+    {
+        _sample = serializedObject.FindProperty("_sample");
+    }
 }
