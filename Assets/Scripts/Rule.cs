f7a6914b3904eaf2c544331b93e40c651be5ab94
Artem Venger <artemvengertilshift@gmail.com>
2022-07-21 20:53:07 +0700

    Реализовал поиск точки на пути, состоящем больше чем из двух точек

--- Assets/Scripts/Rule.cs
+++ Assets/Scripts/Rule.cs
@@ -14,6 +14,11 @@ public class Rule : MonoBehaviour
         {
             _points[i] = transform.GetChild(i);
         }
+
+        if (_points.Length == 0)
+        {
+            Debug.LogError("You didn't add any point!");
+        }
     }
 
     private void Update()
@@ -43,16 +48,39 @@ public class Rule : MonoBehaviour
 
     private Vector3 FindPoint()
     {
+        if (_points.Length == 0)
+        {
+            Debug.LogError("At least one point must exist!");
+            return Vector3.zero;
+        }
+
         if (_distance < 0)
         {
             Debug.LogError("Distance cannot be less then zero!");
             return _points[0].position;
         }
 
-        var vectorBetweenPoints = _points[1].position - _points[0].position;
-        var direction           = Vector3.Normalize(vectorBetweenPoints);
-        var point               = direction * _distance;
+        var restOfDistance = _distance;
 
-        return point;
+        for (var i = 0; i < _points.Length - 1; i++)
+        {
+            var firstPosition  = _points[i].position;
+            var secondPosition = _points[i + 1].position;
+
+            var vectorBetweenPoints   = secondPosition - firstPosition;
+            var distanceBetweenPoints = vectorBetweenPoints.magnitude;
+
+            if (restOfDistance <= distanceBetweenPoints)
+            {
+                var direction = Vector3.Normalize(vectorBetweenPoints);
+                var point     = firstPosition + direction * restOfDistance;
+
+                return point;
+            }
+
+            restOfDistance -= distanceBetweenPoints;
+        }
+
+        return _points[^1].position;
     }
 }
