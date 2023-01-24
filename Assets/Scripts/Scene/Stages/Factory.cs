98fc3305380eb89de980769aa362b7d89b8e69ef
Artem Venger <artemvengertilshift@gmail.com>
2023-01-24 13:18:50 +0700

    Создал фабрику для этапов

---
+++ Assets/Scripts/Scene/Stages/Factory.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Scene.Stages
+{
+    public class Factory
+    {
+        public IStage Create(StageType type)
+        {
+            switch (type)
+            {
+                case StageType.Preparation:
+                    return new Preparation();
+                case StageType.Game:
+                    return new Game();
+                case StageType.Termination:
+                    return new Termination();
+                default:
+                    Debug.LogError($"Can't create IStage for type {type}");
+                    return null;
+            }
+        }
+    }
+}
