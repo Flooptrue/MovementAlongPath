c7e3f31e670fe17e9bae7d96b6c277b69f0e2f0c
Artem Venger <artemvengertilshift@gmail.com>
2023-01-24 13:32:23 +0700

    Создал список этапов сцены

--- Assets/Scripts/Scene/Loop.cs
+++ Assets/Scripts/Scene/Loop.cs
@@ -1,9 +1,32 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using Scene.Stages;
+using UnityEngine;
 
 namespace Scene
 {
-    public class Loop
+    public class Loop : MonoBehaviour
     {
         [SerializeField] private Settings _settings;
+        
+        private List<IStage> _stages;
+        private IStage       _currentStage;
+
+        private void Awake()
+        {
+            _stages = new List<IStage>();
+
+            CreateStages();
+        }
+
+        private void CreateStages()
+        {
+            var factory = new Factory();
+
+            foreach (var stageType in _settings.StagesOrder)
+            {
+                var stage = factory.Create(stageType);
+                _stages.Add(stage);
+            }
+        }
     }
 }
