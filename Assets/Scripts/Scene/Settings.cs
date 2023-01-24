e2de7d56d038ed95b170165f27d736af9eb0ec3c
Artem Venger <artemvengertilshift@gmail.com>
2023-01-24 13:03:45 +0700

    Создал SO с настройками сцены

---
+++ Assets/Scripts/Scene/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Scene.Stages;
+using UnityEngine;
+
+namespace Scene
+{
+    [CreateAssetMenu(fileName = "SceneSettings", menuName = "Road/SceneSettings", order = 0)]
+    public class Settings : ScriptableObject
+    {
+        [field: SerializeField]
+        public List<StageType> StagesOrder;
+    }
+}
