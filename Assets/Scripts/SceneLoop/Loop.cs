479778817a2439407b9bf94b9b0ecd99e2b58804
Artem Venger <artemvengertilshift@gmail.com>
2023-01-24 14:17:33 +0700

    Добавил переключение этапов

--- Assets/Scripts/SceneLoop/Loop.cs
+++ Assets/Scripts/SceneLoop/Loop.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using SceneLoop.Stages;
 using UnityEngine;
 
@@ -15,6 +16,7 @@ namespace SceneLoop
             _stages = new Dictionary<StageType, IStage>();
 
             CreateStages();
+            StartCoroutine(GetRunR(_settings.StagesOrder, _stages));
         }
 
         private void CreateStages()
@@ -25,6 +27,22 @@ namespace SceneLoop
             {
                 var stage = factory.Create(stageType);
                 _stages.Add(stageType, stage);
+            }
+        }
+
+        private IEnumerator GetRunR(List<StageType> stagesOrder, IReadOnlyDictionary<StageType, IStage> stages)
+        {
+            foreach (var stageType in stagesOrder)
+            {
+                var currentStage = stages[stageType];
+                currentStage.Start();
+
+                while (currentStage.IsComplete == false)
+                {
+                    yield return null;
+                }
+
+                currentStage.Finish();
             }
         }
     }
