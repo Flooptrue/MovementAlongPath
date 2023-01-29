91cbf53ba1914c5cb69b75413513aa3042490289
Artem Venger <artemvengertilshift@gmail.com>
2023-01-29 08:50:53 +0700

    Добавил метод расширения для Vector3

---
+++ Assets/Scripts/Extensions/Vector3.cs
@@ -0,0 +1,7 @@
+﻿namespace Extensions
+{
+    public static class Vector3
+    {
+        public static UnityEngine.Vector3 RemoveY(this UnityEngine.Vector3 value) => new(value.x, 0, value.z);
+    }
+}
