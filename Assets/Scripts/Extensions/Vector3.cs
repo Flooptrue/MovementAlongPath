2d344ab9faea6c69befca9713c3650bd4f355007
Artem Venger <artemvengertilshift@gmail.com>
2023-01-29 08:54:05 +0700

    Изменил название класса
    Нельзя было без указания namespace'а вызвать этот метод

--- Assets/Scripts/Extensions/Vector3.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Extensions
-{
-    public static class Vector3
-    {
-        public static UnityEngine.Vector3 RemoveY(this UnityEngine.Vector3 value) => new(value.x, 0, value.z);
-    }
-}
---
+++ Assets/Scripts/Extensions/Vectors.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Extensions
+{
+    public static class Vectors
+    {
+        public static Vector3 RemoveY(this Vector3 value) => new(value.x, 0, value.z);
+    }
+}
