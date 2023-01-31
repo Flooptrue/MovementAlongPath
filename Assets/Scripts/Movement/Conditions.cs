6ec71b9eabc0c79d6b0d241897dfc9daf8f1525b
Artem Venger <artemvengertilshift@gmail.com>
2023-01-31 23:04:47 +0700

    Создал класс с условиями движения

---
+++ Assets/Scripts/Movement/Conditions.cs
@@ -0,0 +1,27 @@
+﻿using Constants;
+using UnityEngine;
+
+namespace Movement
+{
+    public class Conditions
+    {
+        private readonly Input.Movement _input;
+        private readonly float          _maxSlopeAngle;
+
+        public Conditions(Input.Movement input, float maxSlopeAngle)
+        {
+            _input         = input;
+            _maxSlopeAngle = maxSlopeAngle;
+        }
+
+        public bool CanMove(float slopeAngle)
+        {
+            return _input && CanWalkOnSlope(slopeAngle);
+        }
+
+        private bool CanWalkOnSlope(float slopeAngle)
+        {
+            return Mathf.Abs(_maxSlopeAngle - slopeAngle) < Comparison.TOLERANCE;
+        }
+    }
+}
