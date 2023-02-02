16ef27f9dba36ba39eb0c94b19829494926de882
Artem Venger <artemvengertilshift@gmail.com>
2023-02-02 22:08:09 +0700

    Создал структуру с информацией о движении

---
+++ Assets/Scripts/Movement/State.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Movement
+{
+    public readonly struct State
+    {
+        public Vector3 Position { get; }
+        public Vector3 ForwardDirection { get; }
+        public Vector3 Target { get; }
+
+        public State(Vector3 position, Vector3 forwardDirection, Vector3 target)
+        {
+            Position         = position;
+            ForwardDirection = forwardDirection;
+            Target           = target;
+        }
+    }
+}
