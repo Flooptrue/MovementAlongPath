0ebe141941bf5223bc9fcb38de28b0bb3abc7be7
Artem Venger <artemvengertilshift@gmail.com>
2023-02-05 22:18:14 +0700

    Создал класс, ищующий следующую точку

---
+++ Assets/Scripts/Movement/WaypointFinder.cs
@@ -0,0 +1,31 @@
+﻿using Constants;
+using UnityEngine;
+
+namespace Movement
+{
+    public class WaypointFinder
+    {
+        public WaypointFinder(Road road, Transform wanderer)
+        {
+            Road     = road;
+            Wanderer = wanderer;
+            Target   = Road.GetNext(null);
+        }
+
+        private Road      Road     { get; }
+        private Transform Wanderer { get; }
+        private Waypoint  Target   { get; set; }
+
+        public Vector3 TargetPosition => Target.Position;
+
+        public void Update()
+        {
+            var arePositionsEqual = Vector3.Distance(Wanderer.position, TargetPosition) < Comparison.TOLERANCE;
+
+            if (arePositionsEqual && Road.IsLast(Target) == false)
+            {
+                Target = Road.GetNext(Target);
+            }
+        }
+    }
+}
